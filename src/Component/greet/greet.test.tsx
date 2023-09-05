931203bbdd78296f1eb7cc86fc6743f4f8d58b99
Praphulkatlana <praphul.katlana@nagarro.com>
2023-09-05 15:03:59 +0530

    add describe

--- src/Component/greet/greet.test.tsx
+++ src/Component/greet/greet.test.tsx
@@ -13,3 +13,17 @@ test('greeet renders name correctly', () => {
     const textMessage =screen.getByText("hello lucky")
     expect(textMessage).toBeInTheDocument()
 })
+
+describe('greeet',()=>{
+    test('renders correctly', () => {
+        render(<Greet/>)
+        const textMessage =screen.getByText("hello")
+        expect(textMessage).toBeInTheDocument()
+    })
+    
+    test('renders name correctly', () => {
+        render(<Greet name="lucky"/>)
+        const textMessage =screen.getByText("hello lucky")
+        expect(textMessage).toBeInTheDocument()
+    })
+})
